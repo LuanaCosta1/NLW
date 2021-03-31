393356c2cf152c07ef3b597bc8b36514e6d6e5ce
Luana <luana@pop-os.localdomain>
2021-03-30 23:32:50 -0300

    Primeiro commit

---
+++ public/scripts/inro.js
@@ -0,0 +1,70 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+//var myvar // undefined
+//let myvar = "He"
+//const myconst = "He"
+
+//docments.write(myconst + myvar)
+
+// string
+// "ISSO É UMA STRING"
+
+//n number
+//const n1 = 1
+//const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+//const bTrue = true
+//const bFalse = false
+//document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidades
+//const pessoa = {
+//  altura: "1,80m",
+//  idade: 24,
+//  solteira: true,
+//  correr(){
+//    return("run Forest")
+//  }
+//}
+//
+//document.write(pessoa.correr())
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolinhas[3]. name)
+
+// Funçoes - Funcionalidades - Atalhos - Reuso de código
+
+// registrar função
+// function sayMyName(name){
+//   document.write(name)
+// }
+
+// // executar
+// sayMyName("Douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+// condicionais
+
+// const notaFinal = 4
+
+// if( notaFinal < 5 ){
+//   document.write("Reprovado")
+//   //caminho
+// }
+// else{
+//   document.write("Aprovado")
+//   //caminhop 2
+// }
+
+// loop repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write('<p>${i}</p>')
+// }
